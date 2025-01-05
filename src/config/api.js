8c712d995e783e80d6aa18127e25578d1afaa270
prathameshchaskar <prathameshchaskar1@gmail.com>
2025-01-06 00:55:52 +0530

    api config

--- src/config/api.js
+++ src/config/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const LOCALHOST = "http://localhost:5858";
+const LOCALHOST = "https://ecommerce-website-backend-production.up.railway.app/";
 
 export const API_BASE_URL = LOCALHOST
 
